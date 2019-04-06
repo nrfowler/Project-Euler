7b91d89c0e9e9f79715ec39696b261ecb688ab90
nrfowler <nrfowler@gmail.com>
2019-04-06 10:52:42 -0500

    Initial Commit. PE problem #2.

--- Project Euler/Program.cs
+++ Project Euler/Program.cs
@@ -6,7 +6,28 @@ namespace Project_Euler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter problem number");
+            var num = int.Parse(Console.ReadLine());
+
+            if (num == 1)
+            {
+                
+            }
+            else if (num == 2)
+            {
+                long fibn = 1, pfibn=1,sum=0;
+                while (fibn < 4e6)
+                {
+                    if(fibn % 2 == 0)
+                    {
+                        sum += fibn;
+                    }
+                    fibn = fibn + pfibn;
+                    pfibn = fibn;
+                }
+                Console.WriteLine("Programmatic Method: Sum is "+sum);
+            }
+            Console.Read();
         }
     }
 }
